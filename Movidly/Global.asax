6419052a96ebe18f16b9c5b8ee6adecfb6e66476
Teja <teja.maripuri@gmail.com>
2018-05-06 21:06:01 -0500

    Add project files.

---
+++ Movidly/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movidly.MvcApplication" Language="C#" %>
---
+++ Movidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movidly.Startup))]
+namespace Movidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
